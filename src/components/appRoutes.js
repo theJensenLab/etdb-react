f87d88e8c0aa0b5939a33708c89a415e8265ab09
OstlerDev <OstlerDev@users.noreply.github.com>
2018-03-12 13:49:59 -0700

    Add more routes

--- src/components/appRoutes.js
+++ src/components/appRoutes.js
@@ -8,6 +8,10 @@ import ReactDOM from 'react-dom';
 import About from './about.js';
 import Home from './home.js';
 import Search from './search.js';
+import Contact from './contact.js';
+import Featured from './featured.js';
+import Browse from './browse.js';
+import TomogramView from './tomogramView.js';
 
 class AppRoutes extends Component {
 	render(){
@@ -15,6 +19,10 @@ class AppRoutes extends Component {
 			<Switch>
 				<Route path="/about" component={About}/>
 				<Route path="/search" component={Search}/>
+				<Route path="/contact" component={Contact}/>
+				<Route path="/featured" component={Featured}/>
+				<Route path="/browse" component={Browse}/>
+				<Route path="/view" component={TomogramView}/>
 				<Route path="/" component={Home}/>
 			</Switch>
 		)
