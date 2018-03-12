5a11f2b8504ca53e1d808d3b6a443c19bfed1164
OstlerDev <OstlerDev@users.noreply.github.com>
2018-03-12 13:50:56 -0700

    Add TomogramView

---
+++ src/components/tomogramView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TomogramView extends Component {
+	render(){
+		return(
+			<div>
+				TomogramView
+			</div>
+		)
+	}
+}
+
+export default TomogramView;
