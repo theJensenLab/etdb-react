07fe5724e8177636a8f67de58255d09ff0785697
OstlerDev <OstlerDev@users.noreply.github.com>
2018-03-12 13:50:48 -0700

    Update Home

--- src/components/home.js
+++ src/components/home.js
@@ -8,11 +8,17 @@ class Home extends Component {
 		return(
 			<div>
 				<div id="headerfixed">
-				  <div class="transparent" id="topbar">
-				    <div class="row">
-				      <div class="col-sm-6"><a href="./index.js"></a><img class="black-logo" src={logo} /></div>
-				      <div class="col-sm-6">
-				        <div id="menu"><a href="./about">About</a><a href="./database">Browse Database</a><a href="./featured">Featured Tomograms</a><a href="http://www.designing-science-development.com/jensen/about/#faq">FAQ</a><a href="./contact">Contact</a></div>
+				  <div className="transparent" id="topbar">
+				    <div className="row">
+				      <div className="col-sm-6"><a href="./index.js"></a><img className="black-logo" src={logo} /></div>
+				      <div className="col-sm-6">
+				        <div id="menu">
+			          	<a href="/about">About</a>
+			          	<a href="/browse">Browse Database</a>
+			          	<a href="/featured">Featured Tomograms</a>
+			          	<a href="/about#FAQ">FAQ</a>
+			          	<a href="/contact">Contact</a>
+			          </div>
 				      </div>
 				    </div>
 				  </div>
@@ -20,12 +26,12 @@ class Home extends Component {
 				<div id="home-image">
 				  <div id="home-image-container"><img src={red}/></div>
 				</div>
-				<div class="row" id="homecontent">
-				  <div class="col-sm-6" id="homehero"></div>
-				  <div class="col-sm-6" id="homesecondary">
+				<div className="row" id="homecontent">
+				  <div className="col-sm-6" id="homehero"></div>
+				  <div className="col-sm-6" id="homesecondary">
 				    <div>
 				      <h3>Caltech Electron Tomography Database</h3>
-				      <h6>Cryo-electron tomography is a specialized imaging technique used to produce high-resolution, three-dimensional views of cells</h6><a class="browse" href="/database">Browse database <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+				      <h6>Cryo-electron tomography is a specialized imaging technique used to produce high-resolution, three-dimensional views of cells</h6><a className="browse" href="/database">Browse database <i className="fa fa-arrow-right" aria-hidden="true"></i></a>
 				    </div>
 				  </div>
 				</div>
