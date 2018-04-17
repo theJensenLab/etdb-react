16e3898426dd2bad6dc4022688328ea84e051227
Ryan <ryan.chacon@alexandria.io>
2018-04-17 13:08:33 -0700

    conditional rendering update with arrows

---
+++ src/components/flipSort.js
@@ -0,0 +1,28 @@
+
+import React, {Component} from 'react';
+
+class FlipSort extends Component {
+  constructor(props){
+    super(props);
+
+    this.handleFlipState = this.handleFlipState.bind(this);
+  }
+  handleFlipState() {
+    this.props.onFlipChange();
+  }
+
+  render() {
+    const flipState = this.props.flipState;
+    const flipArrow = flipState ? (
+      <span>▼</span>
+    ) : (
+      <span>▲</span>
+    )
+    return (
+      <button onClick={this.handleFlipState} className="buttonSort">{flipArrow}</button>
+    )
+  }
+}
+
+
+export default FlipSort
--- src/components/sortValueButton.js
+++ src/components/sortValueButton.js
@@ -1,24 +1,38 @@
 
 import React, {Component} from 'react';
-
+import FlipSort from './flipSort';
 
 class SortValueButton extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      flipSort: false
+    }
+
     this.handleSortButtonChange = this.handleSortButtonChange.bind(this);
+    this.handleFlipState = this.handleFlipState.bind(this);
 
   }
 
   handleSortButtonChange(e) {
     this.props.onSortButtonChange(e.target.name);
-    console.log(e.target);
+    console.log(e.target.name);
+    console.log(this.state.flipSort);
+  }
+
+  handleFlipState() {
+    this.setState(prevState => ({
+      flipSort: !prevState.flipSort
+    }))
+    console.log(this.state.flipSort)
   }
 
 
   render() {
     const sortValue = this.props.sortValue;
     const buttonName = this.props.name;
+    //if the button is the same as the state selected
     const sortBool = (sortValue === buttonName);
     const buttonValue = this.props.buttonValue;
 
@@ -31,11 +45,16 @@ class SortValueButton extends Component {
         {buttonValue}
       </button>);
 
+    const flipSort = sortBool ? (
+      <FlipSort onFlipChange={this.handleFlipState} flipState={this.state.flipSort} />
+    ) : ( null)
+
     return (
       <div className="row">
-        <div className="col">
+        <div className="col-sm-10">
           {button}
         </div>
+        <div className="col-sm-2">{flipSort}</div>
       </div>
     )
   }
