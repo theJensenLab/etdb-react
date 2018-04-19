4968fcdc37670fae5afc06dcc2577696a3447cc8
Ryan <ryan.chacon@alexandria.io>
2018-04-19 11:25:12 -0700

    added search.js

---
+++ src/components/search.js
@@ -0,0 +1,22 @@
+
+import React, {Component} from 'react';
+
+class Smart extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+
+    }
+
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+
+export default Smart
