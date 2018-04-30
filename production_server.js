e843778befcdab063858c0aeaa385f3cba0140a0
Sky Young <sky@alexandria.io>
2018-04-30 16:33:38 -0700

    For all other assets, serve from `/src`

--- production_server.js
+++ production_server.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.use(express.static('dist'))
 
+app.use('/src', express.static('src'))
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
 
 app.listen(3000, () => console.log('ETDB listening on port 3000!'))
