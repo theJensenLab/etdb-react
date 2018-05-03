d5f4eb974b3b14ced0160793ee0cca04d5858a6d
Davi Ortega <ortegad@caltech.edu>
2018-05-03 16:05:59 -0700

    pixel size alert

--- src/components/termsAndConditions.js
+++ src/components/termsAndConditions.js
@@ -22,7 +22,9 @@ class termsAndConditions extends Component {
 
               <p> If any party wishes to use the images or data for commercial purposes, please contact Prof. Grant Jensen at jensen(at)caltech.edu.  The California Institute of Technology (Caltech) claims copyright to the data and images contained herein under <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png"/></a>.</p>
                         
-              <p>Images and data are provided “as is,” with no warranties of any kind, including that the use of the images and data infringe any third party intellectual property rights.   Note some representations of a small fraction of the data have been published in scientific journals. Some of these journals claim copyright on those particular representations that were shown in the paper as figures, but it is highly unlikely that any new representation would exactly match that already copyrighted.</p>
+              <p>Images and data are provided “as is,” with no warranties of any kind, including that the use of the images and data infringe any third party intellectual property rights. Note some representations of a small fraction of the data have been published in scientific journals. Some of these journals claim copyright on those particular representations that were shown in the paper as figures, but it is highly unlikely that any new representation would exactly match that already copyrighted.</p>
+
+							<p> Also note that the metadata, including pixel size and electron dosage, of individual tomograms may be incorrect. If these measurements affect your use of the data, please <a target="blank" href="/contact">contact us</a> to verify their accuracy.</p>
 				  	</div>        
 				</div>
 				<Footer />
